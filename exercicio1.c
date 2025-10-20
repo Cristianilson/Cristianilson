13250f57b3bbb4395ebf7c3cf70309f0ae914c43
Cristianilson Goes <cristianilsongoes@yahoo.com.br>
2025-10-20 22:41:30 +0000

    finalizado

--- exercicio1.c
+++ exercicio1.c
@@ -1,9 +1,31 @@
 #include <stdio.h>
 
-//Desafio de Exercícios
-// Testando a gravação
-
 int main(){
+    /*cada estado terá uma letra de A a H, tipo char*/ 
+    char Estado[20];
+    char codigo_carta[20];
+    char nome_cidade[20];
+    int população, pontos_turisticos;
+    float area, pib;
+    /*após essa introdução da 1ª carta*/
+    printf("CARTA 1\n");
+    printf("Nome do Estado: SERGIPE\n");
+    printf("Código da Carta: A01\n");
+    printf("Nome da Cidade: ARACAJU\n");
+    printf("População: 800.000 HABITANTES\n");
+    printf("Área: 181.857 Km²\n");
+    printf("PIB: 16.447.105 MIL\n");
+    printf("Pontos Turísticos: 50\n");
+    printf("\n");
+     printf("CARTA 2\n");
+    printf("Nome do Estado: ALAGOAS\n");
+    printf("Código da Carta: B01\n");
+     printf("Nome da Cidade: MACEIÓ\n");
+    printf("População: 960.000 HABITANTES\n");
+    printf("Área: 509.320 Km²\n");
+    printf("PIB: 27.484.016 MIL\n");
+    printf("Pontos Turísticos: 65\n");
+    
 
 
     
